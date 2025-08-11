efee9a1e5b05d78175b2090769ece4d0a093afdf
Pavan Krishna Vedagiri <pavankrishnavedagiri@gmail.com>
2025-08-11 20:42:25 +0530

    feat(user): add POST mapping to create user

--- src/main/java/com/pavan/github/estatemanagementsystem/controllers/UsersController.java
+++ src/main/java/com/pavan/github/estatemanagementsystem/controllers/UsersController.java
@@ -1,9 +1,12 @@
 package com.pavan.github.estatemanagementsystem.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,7 @@ import com.pavan.github.estatemanagementsystem.modals.User;
 @RequestMapping("/api/users")
 public class UsersController {
 	
-	List<User> users = List.of(
+	public List<User> users = new ArrayList<User>(List.of(
 		    new User("U001", "Alice Kumar", "alice.k@example.com", "9876543210",
 		        "pass@123",
 		        new Address("A001", "12A", "MG Road", "Bangalore", "India", "560001")),
@@ -27,7 +30,7 @@ public class UsersController {
 		    new User("U003", "Sneha Reddy", "sneha.r@example.com", "9988776655",
 		        "sneha@789",
 		        new Address("A003", "8C", "Jubilee Hills", "Hyderabad", "India", "500033"))
-		);
+		));
 
 	
 	@GetMapping("/")
@@ -38,6 +41,14 @@ public class UsersController {
 	@GetMapping("/{id}")
 	public User getUserById(@PathVariable String id) throws NoUserFoundException {
 		return users.stream().filter(u -> u.getId().equals(id)).findFirst().orElseThrow(() -> new NoUserFoundException("No user found with given id : " + id));
+	}
+	
+	@PostMapping("/add")
+	public String addUser(@RequestBody User newUser) {
+		System.out.println(newUser);
+		
+		users.add(newUser);
+		return "new user added successfully";
 	}
 
 	
