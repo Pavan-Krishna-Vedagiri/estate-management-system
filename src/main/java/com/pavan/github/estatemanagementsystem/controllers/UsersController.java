5898d24b274d413e89e8a20a91fab313fc782148
Pavan Krishna Vedagiri <pavankrishnavedagiri@gmail.com>
2025-08-13 17:08:49 +0530

    feat(userService) : created user service class and integrated with controller

--- src/main/java/com/pavan/github/estatemanagementsystem/controllers/UsersController.java
+++ src/main/java/com/pavan/github/estatemanagementsystem/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.pavan.github.estatemanagementsystem.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,67 +16,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pavan.github.estatemanagementsystem.exceptions.NoUserFoundException;
 import com.pavan.github.estatemanagementsystem.modals.Address;
 import com.pavan.github.estatemanagementsystem.modals.User;
+import com.pavan.github.estatemanagementsystem.services.UserManagementService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 	
-	public List<User> users = new ArrayList<User>(List.of(
-		    new User("U001", "Alice Kumar", "alice.k@example.com", "9876543210",
-		        "pass@123",
-		        new Address("A001", "12A", "MG Road", "Bangalore", "India", "560001")),
-
-		    new User("U002", "Ravi Mehta", "ravi.m@example.com", "9123456789",
-		        "secure@456",
-		        new Address("A002", "45B", "Park Street", "Kolkata", "India", "700016")),
-
-		    new User("U003", "Sneha Reddy", "sneha.r@example.com", "9988776655",
-		        "sneha@789",
-		        new Address("A003", "8C", "Jubilee Hills", "Hyderabad", "India", "500033"))
-		));
-
+	@Autowired
+	private UserManagementService userManagementService;
 	
 	@GetMapping("/")
 	public List<User> getAllUsers(){
-		return users;
+		return userManagementService.getAllUsers();
 	}
 	
 	@GetMapping("/{id}")
 	public User getUserById(@PathVariable String id) throws NoUserFoundException {
-		return users.stream().filter(u -> u.getId().equals(id)).findFirst().orElseThrow(() -> new NoUserFoundException("No user found with given id : " + id));
+		return userManagementService.getUserById(id);
 	}
 	
 	@PostMapping("/add")
 	public String addUser(@RequestBody User newUser) {
-		System.out.println(newUser);
-		
-		users.add(newUser);
-		return "new user added successfully";
+		return userManagementService.addUser(newUser);
 	}
 	
 	@PutMapping("/update/{id}")
 	public String updateUser(@PathVariable String id ,@RequestBody User updatedUser) throws NoUserFoundException {
-		User existingUser = users.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
-		if(existingUser == null)
-			throw new NoUserFoundException("No user found with given id : " + id);
-		else {			
-			existingUser.setName(updatedUser.getName());
-			existingUser.setAddress(updatedUser.getAddress());
-			existingUser.setEmail(updatedUser.getEmail());
-			existingUser.setPassword(updatedUser.getPassword());
-			existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
-		}	
-		return "User update successfully";
+		return userManagementService.updateUser(id, updatedUser);
 	}
 	
 	@DeleteMapping("/delete/{id}")
 	public String deleteUser(@PathVariable String id) throws NoUserFoundException {
-		User existingUser = users.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
-		if(existingUser == null)
-			throw new NoUserFoundException("No user found with given id : " + id);
-		else {		
-			users.remove(existingUser);
-		}	
-		return "User deleted successfully";
+		return userManagementService.deleteUser(id);
 	}
 }
